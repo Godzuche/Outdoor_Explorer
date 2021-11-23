889411f95c316a36f0229c7c4d64088de134e27d
Administrator <godxuche@gmail.com>
2021-11-23 12:30:16 +0100

    fixed the issue of map not displaying

--- app/src/main/java/com/godzuche/outdoorexplorer/ui/map/MapFragment.kt
+++ app/src/main/java/com/godzuche/outdoorexplorer/ui/map/MapFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.godzuche.outdoorexplorer.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class MapFragment : Fragment() {
 
@@ -20,6 +23,16 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val mapViewModel = ViewModelProvider(this)[MapViewModel::class.java]
 
+
+        //get reference to the google maps fragment in the layout using the fragment manager
+        val maps = childFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        maps.getMapAsync { map ->
+            //lat and lng of San Francisco
+            //we set the bay to San Francisco because all of our locations are within San Fran
+            val bay = LatLng(37.747063, -122.329030)
+            map.moveCamera(CameraUpdateFactory.zoomTo(10f))
+            map.moveCamera(CameraUpdateFactory.newLatLng(bay))
+        }
     }
 
 //    private fun getBitmapFromVector(
